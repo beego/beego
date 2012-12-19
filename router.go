4d4096c7e440fa657e76c4203f75ef8a82fad4b8
astaxie <xiemengjun@gmail.com>
2012-12-19 16:41:39 +0800

    fix previer bug

--- router.go
+++ router.go
@@ -158,7 +158,7 @@ func (p *ControllerRegistor) ServeHTTP(rw http.ResponseWriter, r *http.Request) 
 		}
 
 		if (requestPath[n-1] != '/' && route.pattern == requestPath) ||
-			(len(route.pattern) >= n && requestPath[0:n-1] == route.pattern) {
+			(len(route.pattern) >= n-1 && requestPath[0:n-1] == route.pattern) {
 			runrouter = route
 			findrouter = true
 			break
