895ff49a9efc7caac0bcd10e7758985915f1661e
Unknown <joe2010xtmf@163.com>
2013-03-21 01:17:28 -0400

    add func html2str()

--- template.go
+++ template.go
@@ -54,6 +54,33 @@ func Substr(s string, start, length int) string {
 	return string(bt[start:end])
 }
 
+// Html2str() returns escaping text convert from html
+func Html2str(html string) string {
+	src := string(html)
+
+	//将HTML标签全转换成小写
+	re, _ := regexp.Compile("\\<[\\S\\s]+?\\>")
+	src = re.ReplaceAllStringFunc(src, strings.ToLower)
+
+	//去除STYLE
+	re, _ = regexp.Compile("\\<style[\\S\\s]+?\\</style\\>")
+	src = re.ReplaceAllString(src, "")
+
+	//去除SCRIPT
+	re, _ = regexp.Compile("\\<script[\\S\\s]+?\\</script\\>")
+	src = re.ReplaceAllString(src, "")
+
+	//去除所有尖括号内的HTML代码，并换成换行符
+	re, _ = regexp.Compile("\\<[\\S\\s]+?\\>")
+	src = re.ReplaceAllString(src, "\n")
+
+	//去除连续的换行符
+	re, _ = regexp.Compile("\\s{2,}")
+	src = re.ReplaceAllString(src, "\n")
+
+	return strings.TrimSpace(src)
+}
+
 // DateFormat takes a time and a layout string and returns a string with the formatted date. Used by the template parser as "dateformat"
 func DateFormat(t time.Time, layout string) (datestring string) {
 	datestring = t.Format(layout)
