cf82cfeea21e911c9ffc35d5ccb248d954f8f716
astaxie <xiemengjun@gmail.com>
2013-04-27 22:47:32 +0800

    #45 add support post+_method

--- router.go
+++ router.go
@@ -227,6 +227,7 @@ func (p *ControllerRegistor) ServeHTTP(rw http.ResponseWriter, r *http.Request) 
 	}
 
 	requestPath := r.URL.Path
+	r.ParseMultipartForm()
 
 	//user defined Handler
 	for pattern, c := range p.userHandlers {
@@ -350,17 +351,17 @@ func (p *ControllerRegistor) ServeHTTP(rw http.ResponseWriter, r *http.Request) 
 			if r.Method == "GET" {
 				method = vc.MethodByName("Get")
 				method.Call(in)
-			} else if r.Method == "POST" {
-				method = vc.MethodByName("Post")
-				method.Call(in)
 			} else if r.Method == "HEAD" {
 				method = vc.MethodByName("Head")
 				method.Call(in)
-			} else if r.Method == "DELETE" {
+			} else if r.Method == "DELETE" || (r.Method == "POST" && r.Form.Get("_method") == "delete") {
 				method = vc.MethodByName("Delete")
 				method.Call(in)
-			} else if r.Method == "PUT" {
+			} else if r.Method == "PUT" || (r.Method == "POST" && r.Form.Get("_method") == "put") {
 				method = vc.MethodByName("Put")
+				method.Call(in)
+			} else if r.Method == "POST" {
+				method = vc.MethodByName("Post")
 				method.Call(in)
 			} else if r.Method == "PATCH" {
 				method = vc.MethodByName("Patch")
