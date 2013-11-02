9ccdb310037c137567af24ad69dab3d41292f9b8
astaxie <xiemengjun@gmail.com>
2013-11-02 23:54:17 +0800

    fix #236

--- example/beeapi/controllers/default.go
+++ example/beeapi/controllers/default.go
@@ -19,7 +19,7 @@ func (this *ObjectController) Post() {
 }
 
 func (this *ObjectController) Get() {
-	objectId := this.Ctx.Params[":objectId"]
+	objectId := this.Ctx.Input.Param[":objectId"]
 	if objectId != "" {
 		ob, err := models.GetOne(objectId)
 		if err != nil {
@@ -35,7 +35,7 @@ func (this *ObjectController) Get() {
 }
 
 func (this *ObjectController) Put() {
-	objectId := this.Ctx.Params[":objectId"]
+	objectId := this.Ctx.Input.Param[":objectId"]
 	var ob models.Object
 	json.Unmarshal(this.Ctx.Input.RequestBody, &ob)
 
@@ -49,7 +49,7 @@ func (this *ObjectController) Put() {
 }
 
 func (this *ObjectController) Delete() {
-	objectId := this.Ctx.Params[":objectId"]
+	objectId := this.Ctx.Input.Param[":objectId"]
 	models.Delete(objectId)
 	this.Data["json"] = "delete success!"
 	this.ServeJson()
