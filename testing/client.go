dbf6ca2b4cec26ee4a1e4728fa9ef8868c341278
astaxie <xiemengjun@gmail.com>
2013-11-06 16:12:10 +0800

    fix #277

--- testing/client.go
+++ testing/client.go
@@ -8,38 +8,38 @@ import (
 var port = ""
 var baseUrl = "http://localhost:"
 
-type TestHttpRequest struct{
+type TestHttpRequest struct {
 	httplib.BeegoHttpRequest
 }
 
-func getPort() string{
-	if port==""{
-		config,err:= beego.LoadConfig("../conf/app.conf")
-		if err!=nil{
+func getPort() string {
+	if port == "" {
+		config, err := beego.ParseConfig("../conf/app.conf")
+		if err != nil {
 			return "8080"
 		}
-		port=config.String("httpport")
+		port = config.String("httpport")
 		return port
 	}
 	return port
 }
 
 func Get(path string) *TestHttpRequest {
-	return &TestHttpRequest{*httplib.Get(baseUrl+getPort()+path)}
+	return &TestHttpRequest{*httplib.Get(baseUrl + getPort() + path)}
 }
 
 func Post(path string) *TestHttpRequest {
-	return &TestHttpRequest{*httplib.Post(baseUrl+getPort()+path)}
+	return &TestHttpRequest{*httplib.Post(baseUrl + getPort() + path)}
 }
 
 func Put(path string) *TestHttpRequest {
-	return &TestHttpRequest{*httplib.Put(baseUrl+getPort()+path)}
+	return &TestHttpRequest{*httplib.Put(baseUrl + getPort() + path)}
 }
 
 func Delete(path string) *TestHttpRequest {
-	return &TestHttpRequest{*httplib.Delete(baseUrl+getPort()+path)}
+	return &TestHttpRequest{*httplib.Delete(baseUrl + getPort() + path)}
 }
 
 func Head(path string) *TestHttpRequest {
-	return &TestHttpRequest{*httplib.Head(baseUrl+getPort()+path)}
+	return &TestHttpRequest{*httplib.Head(baseUrl + getPort() + path)}
 }
