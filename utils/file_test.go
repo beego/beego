52ebaece738a9feb8e64cd3606193676eb1adf3d
skyblue <ssx205@gmail.com>
2013-12-09 16:01:24 +0800

    add other utils

--- utils/file_test.go
+++ utils/file_test.go
@@ -1,8 +1,12 @@
 package utils
 
 import (
+	"path/filepath"
+	"reflect"
 	"testing"
 )
+
+var noExistedFile = "/tmp/not_existed_file"
 
 func TestSelfPath(t *testing.T) {
 	path := SelfPath()
@@ -10,4 +14,48 @@ func TestSelfPath(t *testing.T) {
 		t.Error("path cannot be empty")
 	}
 	t.Logf("SelfPath: %s", path)
+}
+
+func TestSelfDir(t *testing.T) {
+	dir := SelfDir()
+	t.Logf("SelfDir: %s", dir)
+}
+
+func TestFileExists(t *testing.T) {
+	if !FileExists("/bin/echo") {
+		t.Errorf("/bin/echo should exists, but it didn't")
+	}
+
+	if FileExists(noExistedFile) {
+		t.Errorf("Wierd, how could this file exists: %s", noExistedFile)
+	}
+}
+
+func TestLookFile(t *testing.T) {
+	path, err := LookFile(filepath.Base(SelfPath()), SelfDir())
+	if err != nil {
+		t.Error(err)
+	}
+	t.Log(path)
+
+	path, err = LookFile(noExistedFile, ".")
+	if err == nil {
+		t.Errorf("err shouldnot be nil, got path: %s", SelfDir())
+	}
+}
+
+func TestGrepE(t *testing.T) {
+	_, err := GrepE("", noExistedFile)
+	if err == nil {
+		t.Error("expect file-not-existed error, but got nothing")
+	}
+
+	path := filepath.Join(".", "testdata", "grepe.test")
+	lines, err := GrepE(`^\s*[^#]+`, path)
+	if err != nil {
+		t.Error(err)
+	}
+	if !reflect.DeepEqual(lines, []string{"hello", "world"}) {
+		t.Errorf("expect [hello world], but receive %v", lines)
+	}
 }
