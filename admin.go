ecfd11adb4a16d26d41bcf8802f9bd0e6c81220f
astaxie <xiemengjun@gmail.com>
2013-12-29 11:01:19 +0800

    fix typo healthcheck url

--- admin.go
+++ admin.go
@@ -51,7 +51,7 @@ func AdminIndex(rw http.ResponseWriter, r *http.Request) {
 	rw.Write([]byte("There are servral functions:\n"))
 	rw.Write([]byte("1. Record all request and request time, http://localhost:8088/qps\n"))
 	rw.Write([]byte("2. Get runtime profiling data by the pprof, http://localhost:8088/prof\n"))
-	rw.Write([]byte("3. Get healthcheck result from http://localhost:8088/prof\n"))
+	rw.Write([]byte("3. Get healthcheck result from http://localhost:8088/healthcheck\n"))
 	rw.Write([]byte("4. Get current task infomation from taskhttp://localhost:8088/task \n"))
 	rw.Write([]byte("5. To run a task passed a param http://localhost:8088/runtask\n"))
 	rw.Write([]byte("6. Get all confige & router infomation http://localhost:8088/listconf\n"))
