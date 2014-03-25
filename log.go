4f819dbd9a05b1cbbc579951daae3eaa9b59d3df
asta.xie <asta.xie@zalora.com>
2014-03-26 00:06:25 +0800

    Add a function SetLogFuncCall to enable

--- log.go
+++ log.go
@@ -22,6 +22,11 @@ func SetLevel(l int) {
 	BeeLogger.SetLevel(l)
 }
 
+func SetLogFuncCall(b bool) {
+	BeeLogger.EnableFuncCallDepth(b)
+	BeeLogger.SetLogFuncCallDepth(3)
+}
+
 // logger references the used application logger.
 var BeeLogger *logs.BeeLogger
 
