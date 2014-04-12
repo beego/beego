c188cbbcb4909c8846c0f4c69d369deb85c74330
astaxie <xiemengjun@gmail.com>
2014-04-12 13:18:18 +0800

    update all files License

--- toolbox/debug.go
+++ toolbox/debug.go
@@ -1,4 +1,9 @@
-// Here are the features: healthcheck, profile, statistics and task.
+// Beego (http://beego.me/)
+// @description beego is an open-source, high-performance web framework for the Go programming language.
+// @link        http://github.com/astaxie/beego for the canonical source repository
+// @license     http://github.com/astaxie/beego/blob/master/LICENSE
+// @authors     astaxie
+
 package toolbox
 
 import (
