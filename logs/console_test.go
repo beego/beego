7d09ac252ac2064cdb76351868c7b294fcbea1c2
Frederic G. MARAND <fgm@osinet.fr>
2014-07-11 11:15:34 +0200

    Issue #682: convert logs package to RFC5424 logging levels.

--- logs/console_test.go
+++ logs/console_test.go
@@ -13,22 +13,29 @@ import (
 	"testing"
 )
 
+// Try each log level in decreasing order of priority.
+func testConsoleCalls(bl *BeeLogger) {
+	bl.Emergency("emergency")
+	bl.Alert("alert")
+	bl.Critical("critical")
+	bl.Error("error")
+	bl.Warning("warning")
+	bl.Notice("notice")
+	bl.Informational("informational")
+	bl.Debug("debug")
+}
+
+// Test console logging by visually comparing the lines being output with and
+// without a log level specification.
 func TestConsole(t *testing.T) {
-	log := NewLogger(10000)
-	log.EnableFuncCallDepth(true)
-	log.SetLogger("console", "")
-	log.Trace("trace")
-	log.Info("info")
-	log.Warn("warning")
-	log.Debug("debug")
-	log.Critical("critical")
+	log1 := NewLogger(10000)
+	log1.EnableFuncCallDepth(true)
+	log1.SetLogger("console", "")
+	testConsoleCalls(log1)
+
 	log2 := NewLogger(100)
-	log2.SetLogger("console", `{"level":1}`)
-	log.Trace("trace")
-	log.Info("info")
-	log.Warn("warning")
-	log.Debug("debug")
-	log.Critical("critical")
+	log2.SetLogger("console", `{"level":3}`)
+	testConsoleCalls(log2)
 }
 
 func BenchmarkConsole(b *testing.B) {
@@ -36,6 +43,6 @@ func BenchmarkConsole(b *testing.B) {
 	log.EnableFuncCallDepth(true)
 	log.SetLogger("console", "")
 	for i := 0; i < b.N; i++ {
-		log.Trace("trace")
+		log.Debug("debug")
 	}
 }
