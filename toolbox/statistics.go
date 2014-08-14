c347dd9e7b46c7ca24a29b48c0ec8f9d47c2d822
Lei Cao <lei.cao@zalora.com>
2014-08-14 23:50:15 +0800

    Fix the comments and deleted the println

--- toolbox/statistics.go
+++ toolbox/statistics.go
@@ -1,7 +1,11 @@
 // Beego (http://beego.me/)
+//
 // @description beego is an open-source, high-performance web framework for the Go programming language.
+//
 // @link        http://github.com/astaxie/beego for the canonical source repository
+//
 // @license     http://github.com/astaxie/beego/blob/master/LICENSE
+//
 // @authors     astaxie
 package toolbox
 
@@ -95,7 +99,6 @@ func (m *UrlMap) GetMap() [][]string {
 			resultLists = append(resultLists, result)
 		}
 	}
-	fmt.Println(resultLists)
 	return resultLists
 }
 
