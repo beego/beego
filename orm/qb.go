29b7ff84e1a6e9c37b29aa63d7290a84082a8a6a
Zheng Yang <zhengyang4k@gmail.com>
2014-09-09 14:17:12 +0800

    more complete support for sql language

--- orm/qb.go
+++ orm/qb.go
@@ -19,15 +19,22 @@ import "errors"
 type QueryBuilder interface {
 	Select(fields ...string) QueryBuilder
 	From(tables ...string) QueryBuilder
-	Where(cond string) QueryBuilder
-	LimitOffset(limit int, offset int) QueryBuilder
 	InnerJoin(table string) QueryBuilder
 	LeftJoin(table string) QueryBuilder
+	RightJoin(table string) QueryBuilder
 	On(cond string) QueryBuilder
+	Where(cond string) QueryBuilder
 	And(cond string) QueryBuilder
 	Or(cond string) QueryBuilder
 	In(vals ...string) QueryBuilder
-	Subquery(query string, rename string) string
+	OrderBy(fields ...string) QueryBuilder
+	Asc() QueryBuilder
+	Desc() QueryBuilder
+	Limit(limit int) QueryBuilder
+	Offset(offset int) QueryBuilder
+	GroupBy(fields ...string) QueryBuilder
+	Having(cond string) QueryBuilder
+	Subquery(sub string, alias string) string
 	String() string
 }
 
