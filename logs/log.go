2288ac868c49cadb170fb8876e14a2a188001d96
astaxie <xiemengjun@gmail.com>
2014-11-03 16:34:36 +0800

    remove the deep Caller

--- logs/log.go
+++ logs/log.go
@@ -155,9 +155,6 @@ func (bl *BeeLogger) writerMsg(loglevel int, msg string) error {
 	lm.level = loglevel
 	if bl.enableFuncCallDepth {
 		_, file, line, ok := runtime.Caller(bl.loggerFuncCallDepth + 1)
-		if !ok {
-			_, file, line, ok = runtime.Caller(bl.loggerFuncCallDepth)
-		}
 		if ok {
 			_, filename := path.Split(file)
 			lm.msg = fmt.Sprintf("[%s:%d] %s", filename, line, msg)
