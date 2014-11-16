8de89e44e452a52b0ed40cb8acacc6ed761fbd42
Drew Wells <drew.wells00@gmail.com>
2014-11-16 01:05:52 -0600

    Remove extraneous output from input test

--- context/input_test.go
+++ context/input_test.go
@@ -15,7 +15,6 @@
 package context
 
 import (
-	"fmt"
 	"net/http"
 	"testing"
 )
@@ -30,35 +29,30 @@ func TestParse(t *testing.T) {
 	if id != 123 || err != nil {
 		t.Fatal("id should has int value")
 	}
-	fmt.Println(id)
 
 	var isok bool
 	err = beegoInput.Bind(&isok, "isok")
 	if !isok || err != nil {
 		t.Fatal("isok should be true")
 	}
-	fmt.Println(isok)
 
 	var float float64
 	err = beegoInput.Bind(&float, "ft")
 	if float != 1.2 || err != nil {
 		t.Fatal("float should be equal to 1.2")
 	}
-	fmt.Println(float)
 
 	ol := make([]int, 0, 2)
 	err = beegoInput.Bind(&ol, "ol")
 	if len(ol) != 2 || err != nil || ol[0] != 1 || ol[1] != 2 {
 		t.Fatal("ol should has two elements")
 	}
-	fmt.Println(ol)
 
 	ul := make([]string, 0, 2)
 	err = beegoInput.Bind(&ul, "ul")
 	if len(ul) != 2 || err != nil || ul[0] != "str" || ul[1] != "array" {
 		t.Fatal("ul should has two elements")
 	}
-	fmt.Println(ul)
 
 	type User struct {
 		Name string
@@ -68,5 +62,4 @@ func TestParse(t *testing.T) {
 	if err != nil || user.Name != "astaxie" {
 		t.Fatal("user should has name")
 	}
-	fmt.Println(user)
 }
