6c3e274b6ec143a3325c185247a8da8459d0925c
JessonChan <chinachendongpo@gmail.com>
2015-04-26 16:10:18 +0800

    set default timeout

--- httplib/httplib.go
+++ httplib/httplib.go
@@ -358,10 +358,10 @@ func (b *BeegoHttpRequest) getResponse() (*http.Response, error) {
 	trans := b.setting.Transport
 
 	if b.setting.ConnectTimeout == 0 {
-		b.setting.ConnectTimeout = 30 * time.Second
+		b.setting.ConnectTimeout = 60 * time.Second
 	}
 	if b.setting.ReadWriteTimeout == 0 {
-		b.setting.ReadWriteTimeout = 30 * time.Second
+		b.setting.ReadWriteTimeout = 60 * time.Second
 	}
 
 	if trans == nil {
