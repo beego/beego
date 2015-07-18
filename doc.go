a64305e8372beb4b265a8864e4d1af54ee1528f5
Mawuli Adzoe <mawuli.kofi.mawuli@gmail.com>
2015-07-18 10:14:19 +0000

    Fix indentation

--- doc.go
+++ doc.go
@@ -2,12 +2,12 @@ package beego
 /*
 beego is an open-source, high-performance, modularity, full-stack web framework
 
- package main
- import "github.com/astaxie/beego"
+  package main
+  import "github.com/astaxie/beego"
 
- func main() {
-  beego.Run()
- }
+  func main() {
+   beego.Run()
+  }
 
 more infomation: http://beego.me
 */
