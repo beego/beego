26a27a162125b6dcd1a01c5c0114e288c8878b60
Mawuli Adzoe <mawuli.kofi.mawuli@gmail.com>
2015-07-18 10:09:22 +0000

    Fix #1269 extract documentation

---
+++ doc.go
@@ -0,0 +1,17 @@
+/*
+beego is an open-source, high-performance, modular,
+full-stack web framework
+
+	package main
+
+
+	import "github.com/astaxie/beego"
+
+	func main() {
+	 beego.Run()
+	}
+
+
+more infomation: http://beego.me
+*/
+package beego
