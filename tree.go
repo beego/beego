5ad999a3d177832162c1c3bbc164c652208c7d85
Yongzheng Lai <sdrzlyz@126.com>
2015-09-06 12:01:50 +0800

    Update tree.go
    fix routers for:
    ```
    /topic/:id/?:auth
    /topic/:id/?:auth:int
    ```

--- tree.go
+++ tree.go
@@ -208,6 +208,11 @@ func (t *Tree) addseg(segments []string, route interface{}, wildcards []string, 
 		} else {
 			t.leaves = append(t.leaves, &leafInfo{runObject: route, wildcards: wildcards})
 		}
+		for i, v := range wildcards {
+			if v==":" && i!=0{
+				t.leaves = append(t.leaves, &leafInfo{runObject: route, wildcards: wildcards[:i+1]})
+			}
+		}
 	} else {
 		seg := segments[0]
 		iswild, params, regexpStr := splitSegment(seg)
