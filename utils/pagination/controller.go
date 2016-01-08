302b1ef7df054aec5eec6e2371646c4b372940d1
astaxie <xiemengjun@gmail.com>
2016-01-08 15:52:57 +0800

    fix the data

--- utils/pagination/controller.go
+++ utils/pagination/controller.go
@@ -21,6 +21,6 @@ import (
 // SetPaginator Instantiates a Paginator and assigns it to context.Input.Data["paginator"].
 func SetPaginator(context *context.Context, per int, nums int64) (paginator *Paginator) {
 	paginator = NewPaginator(context.Request, per, nums)
-	context.Input.Data["paginator"] = &paginator
+	context.Input.SetData("paginator", &paginator)
 	return
 }
