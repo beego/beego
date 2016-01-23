ecf24640fd0f62c69bcba8e2099903f2e06dd7e5
youngsterxyf <sas.198708@gmail.com>
2016-01-23 16:24:58 +0800

    fix issue #1566

--- logs/es/es.go
+++ logs/es/es.go
@@ -48,16 +48,16 @@ func (el *esLogger) Init(jsonconfig string) error {
 }
 
 // WriteMsg will write the msg and level into es
-func (el *esLogger) WriteMsg(msg string, level int) error {
+func (el *esLogger) WriteMsg(when time.Time, msg string, level int) error {
 	if level > el.Level {
 		return nil
 	}
-	t := time.Now()
+
 	vals := make(map[string]interface{})
-	vals["@timestamp"] = t.Format(time.RFC3339)
+	vals["@timestamp"] = when.Format(time.RFC3339)
 	vals["@msg"] = msg
 	d := goes.Document{
-		Index:  fmt.Sprintf("%04d.%02d.%02d", t.Year(), t.Month(), t.Day()),
+		Index:  fmt.Sprintf("%04d.%02d.%02d", when.Year(), when.Month(), when.Day()),
 		Type:   "logs",
 		Fields: vals,
 	}
