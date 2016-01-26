beego/beego

commit ad6547936e038759488f5a24823fba4baba9a077

fix the http: multiple response.WriteHeader calls

plugins/cors/cors.go
@@ -24,7 +24,7 @@
 //		// - PUT and PATCH methods
 //		// - Origin header
 //		// - Credentials share
-//		beego.InsertFilter("*", beego.BeforeRouter,cors.Allow(&cors.Options{
+//		beego.InsertFilter("*", beego.BeforeRouter, cors.Allow(&cors.Options{
 //			AllowOrigins:     []string{"https://*.foo.com"},
 //			AllowMethods:     []string{"PUT", "PATCH"},
 //			AllowHeaders:     []string{"Origin"},
@@ -36,7 +36,6 @@
 package cors
 
 import (
-	"net/http"
 	"regexp"
 	"strconv"
 	"strings"
@@ -216,8 +215,6 @@ func Allow(opts *Options) beego.FilterFunc {
 			for key, value := range headers {
 				ctx.Output.Header(key, value)
 			}
-			ctx.Output.SetStatus(http.StatusOK)
-			ctx.WriteString("")
 			return
 		}
 		headers = opts.Header(origin)

plugins/apiauth/apiauth.go
@@ -83,41 +83,41 @@ func APIBaiscAuth(appid, appkey string) beego.FilterFunc {
 func APIAuthWithFunc(f AppIdToAppSecret, timeout int) beego.FilterFunc {
 	return func(ctx *context.Context) {
 		if ctx.Input.Query("appid") == "" {
-			ctx.Output.SetStatus(403)
+			ctx.ResponseWriter.WriteHeader(403)
 			ctx.WriteString("miss query param: appid")
 			return
 		}
 		appsecret := f(ctx.Input.Query("appid"))
 		if appsecret == "" {
-			ctx.Output.SetStatus(403)
+			ctx.ResponseWriter.WriteHeader(403)
 			ctx.WriteString("not exist this appid")
 			return
 		}
 		if ctx.Input.Query("signature") == "" {
-			ctx.Output.SetStatus(403)
+			ctx.ResponseWriter.WriteHeader(403)
 			ctx.WriteString("miss query param: signature")
 			return
 		}
 		if ctx.Input.Query("timestamp") == "" {
-			ctx.Output.SetStatus(403)
+			ctx.ResponseWriter.WriteHeader(403)
 			ctx.WriteString("miss query param: timestamp")
 			return
 		}
 		u, err := time.Parse("2006-01-02 15:04:05", ctx.Input.Query("timestamp"))
 		if err != nil {
-			ctx.Output.SetStatus(403)
+			ctx.ResponseWriter.WriteHeader(403)
 			ctx.WriteString("timestamp format is error, should 2006-01-02 15:04:05")
 			return
 		}
 		t := time.Now()
 		if t.Sub(u).Seconds() > float64(timeout) {
-			ctx.Output.SetStatus(403)
+			ctx.ResponseWriter.WriteHeader(403)
 			ctx.WriteString("timeout! the request time is long ago, please try again")
 			return
 		}
 		if ctx.Input.Query("signature") !=
 			Signature(appsecret, ctx.Input.Method(), ctx.Request.Form, ctx.Input.Uri()) {
-			ctx.Output.SetStatus(403)
+			ctx.ResponseWriter.WriteHeader(403)
 			ctx.WriteString("auth failed")
 		}
 	}

commit e3810b599d10946613001bc5a498e606c911018f

Fix regression caused by commit ad65479
Commit ad65479 will cause "Method Not Allow" in preflight response
when enable CORS plugin.

The root cause is that CORS plugin didn't generate http output after applied
commit ad65479, so the value of `ctx.ResponseWriter.Started` will be keep
`false`, and then later filter chains will be go on to run when CORS filter
finished.

This path will both fix "Method Not Allow" and the original bug
"multiple response.WriteHeader calls".

Signed-off-by: Yunkai Zhang <qiushu.zyk@taobao.com>

plugins/cors/cors.go
@@ -36,6 +36,7 @@
 package cors
 
 import (
+	"net/http"
 	"regexp"
 	"strconv"
 	"strings"
@@ -215,6 +216,7 @@ func Allow(opts *Options) beego.FilterFunc {
 			for key, value := range headers {
 				ctx.Output.Header(key, value)
 			}
+			ctx.ResponseWriter.WriteHeader(http.StatusOK)
 			return
 		}
 		headers = opts.Header(origin)