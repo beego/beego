71f0101d625b308b0e46d74212ba176c38a6df83
Mickaël GALLARD <mlgd17@gmail.com>
2016-02-22 13:26:22 +0100

    Fix cookies in accordance with the "net / http" and Flash usage

--- context/output.go
+++ context/output.go
@@ -97,10 +97,12 @@ func (output *BeegoOutput) Cookie(name string, value string, others ...interface
 			maxAge = v
 		}
 
-		if maxAge > 0 {
+		switch {
+		case maxAge > 0:
 			fmt.Fprintf(&b, "; Expires=%s; Max-Age=%d", time.Now().Add(time.Duration(maxAge)*time.Second).UTC().Format(time.RFC1123), maxAge)
-		} else {
+		case maxAge < 0:
 			fmt.Fprintf(&b, "; Max-Age=0")
+
 		}
 	}
 
