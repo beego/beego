ab22f6e3c35db9fda992dab74d4522a4bd9a8f2c
sheng <sheng@aofei.org>
2016-02-24 02:15:41 +0800

    using low version syntax to rewrite the fix

--- context/output.go
+++ context/output.go
@@ -97,10 +97,9 @@ func (output *BeegoOutput) Cookie(name string, value string, others ...interface
 			maxAge = v
 		}
 
-		switch {
-		case maxAge > 0:
+		if maxAge > 0 {
 			fmt.Fprintf(&b, "; Expires=%s; Max-Age=%d", time.Now().Add(time.Duration(maxAge)*time.Second).UTC().Format(time.RFC1123), maxAge)
-		case maxAge < 0:
+		} else if maxAge < 0 {
 			fmt.Fprintf(&b, "; Max-Age=0")
 		}
 	}
