4bd656f79f7e56e96851db27d8861304e7434d12
sheng <sheng@aofei.org>
2016-02-23 22:18:16 +0800

    fix the cookie's max-age problem again

--- context/output.go
+++ context/output.go
@@ -97,8 +97,11 @@ func (output *BeegoOutput) Cookie(name string, value string, others ...interface
 			maxAge = v
 		}
 
-		if maxAge != 0 {
+		switch {
+		case maxAge > 0:
 			fmt.Fprintf(&b, "; Expires=%s; Max-Age=%d", time.Now().Add(time.Duration(maxAge)*time.Second).UTC().Format(time.RFC1123), maxAge)
+		case maxAge < 0:
+			fmt.Fprintf(&b, "; Max-Age=0")
 		}
 	}
 
