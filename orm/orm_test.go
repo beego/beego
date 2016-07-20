0d5f80f7799224dbb32278d7fe022d94b29c6f00
“fudali113” <fudali113@gmail.com>
2016-07-20 13:21:22 +0800

    update

--- orm/orm_test.go
+++ orm/orm_test.go
@@ -724,42 +724,69 @@ func TestInsertOrUpdate(t *testing.T) {
 	user1 := User{UserName: "u", Status: 2, Password: "o"}
 	user2 := User{UserName: "u", Status: 3, Password: "oo"}
 	dORM.Insert(&user)
-
+	if dORM.Driver().Name() == "sqlite3" {
+		fmt.Println("sqlite3 is nonsupport")
+		return
+	}
 	//test1  普通操作
 	_, err := dORM.InsertOrUpdate(&user1, "UserName")
-	throwFailNow(t, err)
+	if err.Error() == "postgres version must 9.5 or higher" {
+		fmt.Println(err)
+	} else {
+		throwFailNow(t, err)
+	}
 	test := User{UserName: "u"}
 	time.Sleep(time.Second * 1)
 	dORM.Read(&test, "UserName")
 	throwFailNow(t, AssertIs(user1.Status, test.Status))
 	//test2  普通操作
 	_, err = dORM.InsertOrUpdate(&user2, "UserName")
-	throwFailNow(t, err)
+	if err.Error() == "postgres version must 9.5 or higher" {
+		fmt.Println(err)
+	} else {
+		throwFailNow(t, err)
+	}
 	time.Sleep(time.Second * 1)
 	dORM.Read(&test, "UserName")
 	throwFailNow(t, AssertIs(user2.Status, test.Status))
 	throwFailNow(t, AssertIs(user2.Password, strings.TrimSpace(test.Password)))
 	//test3  数字 + 操作
 	_, err = dORM.InsertOrUpdate(&user2, "UserName", "Status=Status+1")
-	throwFailNow(t, err)
+	if err.Error() == "postgres version must 9.5 or higher" {
+		fmt.Println(err)
+	} else {
+		throwFailNow(t, err)
+	}
 	time.Sleep(time.Second * 1)
 	dORM.Read(&test, "UserName")
 	throwFailNow(t, AssertIs(user2.Status+1, test.Status))
 	//test4  数字 - 操作
 	_, err = dORM.InsertOrUpdate(&user2, "UserName", "Status=Status-1")
-	throwFailNow(t, err)
+	if err.Error() == "postgres version must 9.5 or higher" {
+		fmt.Println(err)
+	} else {
+		throwFailNow(t, err)
+	}
 	time.Sleep(time.Second * 1)
 	dORM.Read(&test, "UserName")
 	throwFailNow(t, AssertIs((user2.Status+1)-1, test.Status))
 	//test5  数字 * 操作
 	_, err = dORM.InsertOrUpdate(&user2, "UserName", "Status=Status*3")
-	throwFailNow(t, err)
+	if err.Error() == "postgres version must 9.5 or higher" {
+		fmt.Println(err)
+	} else {
+		throwFailNow(t, err)
+	}
 	time.Sleep(time.Second * 1)
 	dORM.Read(&test, "UserName")
 	throwFailNow(t, AssertIs(((user2.Status+1)-1)*3, test.Status))
 	//test6  数字 / 操作
 	_, err = dORM.InsertOrUpdate(&user2, "UserName", "Status=Status/3")
-	throwFailNow(t, err)
+	if err.Error() == "postgres version must 9.5 or higher" {
+		fmt.Println(err)
+	} else {
+		throwFailNow(t, err)
+	}
 	time.Sleep(time.Second * 1)
 	dORM.Read(&test, "UserName")
 	throwFailNow(t, AssertIs((((user2.Status+1)-1)*3)/3, test.Status))
