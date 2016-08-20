598e4c5b2dbbec632c6f817250768bb5bfc6f123
maxin <maxinvc@126.com>
2016-08-20 20:33:03 +0800

    reset session.go

--- session/session.go
+++ session/session.go
@@ -194,18 +194,17 @@ func (manager *Manager) SessionStart(w http.ResponseWriter, r *http.Request) (se
 		return nil, errs
 	}
 
-	if sid == "" || !manager.provider.SessionExist(sid) {
-		// Generate a new session
-		sid, errs = manager.sessionID()
-		if errs != nil {
-			return nil, errs
-		}
+	if sid != "" && manager.provider.SessionExist(sid) {
+		return manager.provider.SessionRead(sid)
+	}
+
+	// Generate a new session
+	sid, errs = manager.sessionID()
+	if errs != nil {
+		return nil, errs
 	}
 
 	session, err = manager.provider.SessionRead(sid)
-	if err != nil {
-		return nil, errs
-	}
 	cookie := &http.Cookie{
 		Name:     manager.config.CookieName,
 		Value:    url.QueryEscape(sid),
