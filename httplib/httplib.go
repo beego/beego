3742d1178ca9b2c429ed8fe91360002a941d4b89
astaxie <xiemengjun@gmail.com>
2017-04-25 20:42:43 +0800

    httplib support delete params fix #2593

--- httplib/httplib.go
+++ httplib/httplib.go
@@ -344,7 +344,7 @@ func (b *BeegoHTTPRequest) buildURL(paramBody string) {
 	}
 
 	// build POST/PUT/PATCH url and body
-	if (b.req.Method == "POST" || b.req.Method == "PUT" || b.req.Method == "PATCH") && b.req.Body == nil {
+	if (b.req.Method == "POST" || b.req.Method == "PUT" || b.req.Method == "PATCH" || b.req.Method == "DELETE") && b.req.Body == nil {
 		// with files
 		if len(b.files) > 0 {
 			pr, pw := io.Pipe()
