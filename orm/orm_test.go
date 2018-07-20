28dce372505f11fe36b6045cb0845bc1080d521d
Penghui Liao <liaoishere@gmail.com>
2018-07-20 16:51:36 +0800

    fix test case that calls All()
    Signed-off-by: Penghui Liao <liaoishere@gmail.com>

--- orm/orm_test.go
+++ orm/orm_test.go
@@ -1011,13 +1011,13 @@ func TestAll(t *testing.T) {
 
 	qs = dORM.QueryTable("user")
 	num, err = qs.Filter("user_name", "nothing").All(&users)
-	throwFailNow(t, err)
+	throwFail(t, AssertIs(err, ErrNoRows))
 	throwFailNow(t, AssertIs(num, 0))
 
 	var users3 []*User
 	qs = dORM.QueryTable("user")
 	num, err = qs.Filter("user_name", "nothing").All(&users3)
-	throwFailNow(t, err)
+	throwFail(t, AssertIs(err, ErrNoRows))
 	throwFailNow(t, AssertIs(num, 0))
 	throwFailNow(t, AssertIs(users3 == nil, false))
 }
