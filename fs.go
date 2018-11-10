872b787e6c6e076129130052de74b50e77ec6a66
Viktor Vassilyev <lxshadowxkingxl@gmail.com>
2018-11-10 12:34:53 +0600

    refactor(FileSystem): add comments function

--- fs.go
+++ fs.go
@@ -13,6 +13,9 @@ func (d FileSystem) Open(name string) (http.File, error) {
 	return os.Open(name)
 }
 
+// Walk walks the file tree rooted at root in filesystem, calling walkFn for each file or
+// directory in the tree, including root. All errors that arise visiting files
+// and directories are filtered by walkFn.
 func Walk(fs http.FileSystem, root string, walkFn filepath.WalkFunc) error {
 
 	f, err := fs.Open(root)
