12fdc04f1b2c9e9ea1c9c2a636ef71141f342818
Witaya Tospitakkul <witaya1412@gmail.com>
2019-01-25 00:15:40 +0700

    fix: when parse post form it didnt parse fields which have same name but the first index is empty but another is not

--- templatefunc.go
+++ templatefunc.go
@@ -85,24 +85,24 @@ func DateFormat(t time.Time, layout string) (datestring string) {
 var datePatterns = []string{
 	// year
 	"Y", "2006", // A full numeric representation of a year, 4 digits   Examples: 1999 or 2003
-	"y", "06",   //A two digit representation of a year   Examples: 99 or 03
+	"y", "06", //A two digit representation of a year   Examples: 99 or 03
 
 	// month
-	"m", "01",      // Numeric representation of a month, with leading zeros 01 through 12
-	"n", "1",       // Numeric representation of a month, without leading zeros   1 through 12
-	"M", "Jan",     // A short textual representation of a month, three letters Jan through Dec
+	"m", "01", // Numeric representation of a month, with leading zeros 01 through 12
+	"n", "1", // Numeric representation of a month, without leading zeros   1 through 12
+	"M", "Jan", // A short textual representation of a month, three letters Jan through Dec
 	"F", "January", // A full textual representation of a month, such as January or March   January through December
 
 	// day
 	"d", "02", // Day of the month, 2 digits with leading zeros 01 to 31
-	"j", "2",  // Day of the month without leading zeros 1 to 31
+	"j", "2", // Day of the month without leading zeros 1 to 31
 
 	// week
-	"D", "Mon",    // A textual representation of a day, three letters Mon through Sun
+	"D", "Mon", // A textual representation of a day, three letters Mon through Sun
 	"l", "Monday", // A full textual representation of the day of the week  Sunday through Saturday
 
 	// time
-	"g", "3",  // 12-hour format of an hour without leading zeros    1 through 12
+	"g", "3", // 12-hour format of an hour without leading zeros    1 through 12
 	"G", "15", // 24-hour format of an hour without leading zeros   0 through 23
 	"h", "03", // 12-hour format of an hour with leading zeros  01 through 12
 	"H", "15", // 24-hour format of an hour with leading zeros  00 through 23
@@ -297,8 +297,21 @@ func parseFormToStruct(form url.Values, objT reflect.Type, objV reflect.Value) e
 			tag = tags[0]
 		}
 
+		if fieldT.Type.Kind() == reflect.Slice {
+			found := false
+			for _, v := range form[tag] {
+				if len(v) != 0 {
+					found = true
+					break
+				}
+			}
+			if !found {
+				continue
+			}
+		}
+
 		value := form.Get(tag)
-		if len(value) == 0 {
+		if (fieldT.Type.Kind() != reflect.Slice) && len(value) == 0 {
 			continue
 		}
 
