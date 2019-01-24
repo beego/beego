e5947284e031fb651dd55bad5ff7b001ae1574c3
JessonChan <chinachendongpo@gmail.com>
2019-01-24 13:42:35 +0800

    remove ST1017 from staticcheck
    don't use Yoda conditions

--- cache/conv_test.go
+++ cache/conv_test.go
@@ -20,49 +20,49 @@ import (
 
 func TestGetString(t *testing.T) {
 	var t1 = "test1"
-	if "test1" != GetString(t1) {
+	if GetString(t1) != "test1" {
 		t.Error("get string from string error")
 	}
 	var t2 = []byte("test2")
-	if "test2" != GetString(t2) {
+	if GetString(t2) != "test2" {
 		t.Error("get string from byte array error")
 	}
 	var t3 = 1
-	if "1" != GetString(t3) {
+	if GetString(t3) != "1" {
 		t.Error("get string from int error")
 	}
 	var t4 int64 = 1
-	if "1" != GetString(t4) {
+	if GetString(t4) != "1" {
 		t.Error("get string from int64 error")
 	}
 	var t5 = 1.1
-	if "1.1" != GetString(t5) {
+	if GetString(t5) != "1.1" {
 		t.Error("get string from float64 error")
 	}
 
-	if "" != GetString(nil) {
+	if GetString(nil) != "" {
 		t.Error("get string from nil error")
 	}
 }
 
 func TestGetInt(t *testing.T) {
 	var t1 = 1
-	if 1 != GetInt(t1) {
+	if GetInt(t1) != 1 {
 		t.Error("get int from int error")
 	}
 	var t2 int32 = 32
-	if 32 != GetInt(t2) {
+	if GetInt(t2) != 32 {
 		t.Error("get int from int32 error")
 	}
 	var t3 int64 = 64
-	if 64 != GetInt(t3) {
+	if GetInt(t3) != 64 {
 		t.Error("get int from int64 error")
 	}
 	var t4 = "128"
-	if 128 != GetInt(t4) {
+	if GetInt(t4) != 128 {
 		t.Error("get int from num string error")
 	}
-	if 0 != GetInt(nil) {
+	if GetInt(nil) != 0 {
 		t.Error("get int from nil error")
 	}
 }
@@ -70,22 +70,22 @@ func TestGetInt(t *testing.T) {
 func TestGetInt64(t *testing.T) {
 	var i int64 = 1
 	var t1 = 1
-	if i != GetInt64(t1) {
+	if GetInt64(t1) != i {
 		t.Error("get int64 from int error")
 	}
 	var t2 int32 = 1
-	if i != GetInt64(t2) {
+	if GetInt64(t2) != i {
 		t.Error("get int64 from int32 error")
 	}
 	var t3 int64 = 1
-	if i != GetInt64(t3) {
+	if GetInt64(t3) != i {
 		t.Error("get int64 from int64 error")
 	}
 	var t4 = "1"
-	if i != GetInt64(t4) {
+	if GetInt64(t4) != i {
 		t.Error("get int64 from num string error")
 	}
-	if 0 != GetInt64(nil) {
+	if GetInt64(nil) != 0 {
 		t.Error("get int64 from nil")
 	}
 }
@@ -93,25 +93,25 @@ func TestGetInt64(t *testing.T) {
 func TestGetFloat64(t *testing.T) {
 	var f = 1.11
 	var t1 float32 = 1.11
-	if f != GetFloat64(t1) {
+	if GetFloat64(t1) != f {
 		t.Error("get float64 from float32 error")
 	}
 	var t2 = 1.11
-	if f != GetFloat64(t2) {
+	if GetFloat64(t2) != f {
 		t.Error("get float64 from float64 error")
 	}
 	var t3 = "1.11"
-	if f != GetFloat64(t3) {
+	if GetFloat64(t3) != f {
 		t.Error("get float64 from string error")
 	}
 
 	var f2 float64 = 1
 	var t4 = 1
-	if f2 != GetFloat64(t4) {
+	if GetFloat64(t4) != f2 {
 		t.Error("get float64 from int error")
 	}
 
-	if 0 != GetFloat64(nil) {
+	if GetFloat64(nil) != 0 {
 		t.Error("get float64 from nil error")
 	}
 }
