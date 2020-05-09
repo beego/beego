2449aad105e0680a0f53161377d02aafc892582c
jianzhiyao <zhiyao.jian@dreampix.cn>
2020-05-09 10:44:37 +0800

    add cache-hit test example

--- staticfile_test.go
+++ staticfile_test.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"compress/gzip"
 	"compress/zlib"
+	"fmt"
 	"io"
 	"io/ioutil"
 	"os"
@@ -51,6 +52,31 @@ func TestOpenStaticFileDeflate_1(t *testing.T) {
 	content, _ := ioutil.ReadAll(&zipBuf)
 
 	testOpenFile("deflate", content, t)
+}
+
+func TestStaticCacheWork(t *testing.T) {
+	encodings := []string{"", "gzip", "deflate"}
+
+	fi, _ := os.Stat(licenseFile)
+	for _, encoding := range encodings {
+		_, _, first, _, err := openFile(licenseFile, fi, encoding)
+		if err != nil {
+			t.Error(err)
+			continue
+		}
+
+		_, _, second, _, err := openFile(licenseFile, fi, encoding)
+		if err != nil {
+			t.Error(err)
+			continue
+		}
+
+		address1 := fmt.Sprintf("%p", first)
+		address2 := fmt.Sprintf("%p", second)
+		if address1 != address2 {
+			t.Errorf("encoding '%v' can not hit cache", encoding)
+		}
+	}
 }
 
 func assetOpenFileAndContent(sch *serveContentHolder, reader *serveContentReader, content []byte, t *testing.T) {
