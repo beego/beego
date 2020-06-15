ad11d7efabf390007802ac949c89e3fe4fdaed03
harry890829 <wrz29@126.com>
2020-06-15 18:16:40 +0800

    update go test

--- orm/orm_test.go
+++ orm/orm_test.go
@@ -1973,40 +1973,10 @@ func TestUpdate(t *testing.T) {
 	throwFail(t, err)
 	throwFail(t, AssertIs(num, 1))
 
-	num, err = qs.Filter("user_name", "slene").Update(Params{
-		"Nums": ColValue(ColBitAnd, 1),
-	})
-	throwFail(t, err)
-	throwFail(t, AssertIs(num, 1))
-
-	num, err = qs.Filter("user_name", "slene").Update(Params{
-		"Nums": ColValue(ColBitRShift, 1),
-	})
-	throwFail(t, err)
-	throwFail(t, AssertIs(num, 0))
-
-	num, err = qs.Filter("user_name", "slene").Update(Params{
-		"Nums": ColValue(ColBitLShift, 1),
-	})
-	throwFail(t, err)
-	throwFail(t, AssertIs(num, 0))
-
-	num, err = qs.Filter("user_name", "slene").Update(Params{
-		"Nums": ColValue(ColBitXOR, 1),
-	})
-	throwFail(t, err)
-	throwFail(t, AssertIs(num, 1))
-
-	num, err = qs.Filter("user_name", "slene").Update(Params{
-		"Nums": ColValue(ColBitOr, 1),
-	})
-	throwFail(t, err)
-	throwFail(t, AssertIs(num, 0))
-
 	user := User{UserName: "slene"}
 	err = dORM.Read(&user, "UserName")
 	throwFail(t, err)
-	throwFail(t, AssertIs(user.Nums, 1))
+	throwFail(t, AssertIs(user.Nums, 30))
 }
 
 func TestDelete(t *testing.T) {
