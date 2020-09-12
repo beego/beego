870346e4c0672a0fa055276dea704c6567f717e8
l00427301 <liuyang287@huawei.com>
2020-09-12 18:06:59 +0800

    Empty field in validator.Error when label struct tag is not declared #4222

--- validation/validation.go
+++ validation/validation.go
@@ -269,13 +269,15 @@ func (v *Validation) apply(chk Validator, obj interface{}) *Result {
 	Field := ""
 	Label := ""
 	parts := strings.Split(key, ".")
+	if len(parts) == 2 {
+		Field = parts[0]
+		Name = parts[1]
+		Label = Field
+	}
 	if len(parts) == 3 {
 		Field = parts[0]
 		Name = parts[1]
 		Label = parts[2]
-		if len(Label) == 0 {
-			Label = Field
-		}
 	}
 
 	err := &Error{
