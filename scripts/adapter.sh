c793670e90c7aaa38525b73428ca061eb5329241
Ming Deng <mingflycash@gmail.com>
2020-10-08 23:17:38 +0800

    Remove scripts directory; update readme

--- scripts/adapter.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-# using pkg/adapter. Usually you want to migrate to V2 smoothly, you could running this script
-
-find ./ -name '*.go' -type f -exec sed -i '' -e 's/github.com\/astaxie\/beego/github.com\/astaxie\/beego\/pkg\/adapter/g' {} \;
-find ./ -name '*.go' -type f -exec sed -i '' -e 's/"github.com\/astaxie\/beego\/pkg\/adapter"/beego "github.com\/astaxie\/beego\/pkg\/adapter"/g' {} \;
