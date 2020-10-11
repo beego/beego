34d6a733e9be883779fd0a5d70784a3bce2eafe9
Ming Deng <mingflycash@gmail.com>
2020-10-11 23:22:19 +0800

    Support toml config

---
+++ core/config/error.go
@@ -0,0 +1,25 @@
+// Copyright 2020
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package config
+
+import (
+	"github.com/pkg/errors"
+)
+
+// now not all implementation return those error codes
+var (
+	KeyNotFoundError      = errors.New("the key is not found")
+	InvalidValueTypeError = errors.New("the value is not expected type")
+)
