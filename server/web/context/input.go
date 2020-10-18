82d31990625beb942bc7fe266b6011015c6f1cb3
Anker Jam <jianzhiyao020@qq.com>
2020-10-18 23:18:13 +0800

    fix 4224：form entity too large casue run out of memory

--- server/web/context/input.go
+++ server/web/context/input.go
@@ -420,10 +420,11 @@ func (input *BeegoInput) SetData(key, val interface{}) {
 	input.data[key] = val
 }
 
-// ParseFormOrMulitForm parseForm or parseMultiForm based on Content-type
-func (input *BeegoInput) ParseFormOrMulitForm(maxMemory int64) error {
+// ParseFormOrMultiForm parseForm or parseMultiForm based on Content-type
+func (input *BeegoInput) ParseFormOrMultiForm(maxMemory int64) error {
 	// Parse the body depending on the content type.
 	if strings.Contains(input.Header("Content-Type"), "multipart/form-data") {
+		input.Context.Request.Body = http.MaxBytesReader(input.Context.ResponseWriter, input.Context.Request.Body, maxMemory)
 		if err := input.Context.Request.ParseMultipartForm(maxMemory); err != nil {
 			return errors.New("Error parsing request body:" + err.Error())
 		}
