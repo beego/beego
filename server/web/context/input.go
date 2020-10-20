5e42b71812105990b236e73d3aec8e7139dc0302
Ming Deng <mingflycash@gmail.com>
2020-10-20 22:06:24 +0800

    add MaxUploadFile to provide more safety uploading controll

--- server/web/context/input.go
+++ server/web/context/input.go
@@ -423,8 +423,7 @@ func (input *BeegoInput) SetData(key, val interface{}) {
 // ParseFormOrMultiForm parseForm or parseMultiForm based on Content-type
 func (input *BeegoInput) ParseFormOrMultiForm(maxMemory int64) error {
 	// Parse the body depending on the content type.
-	input.Context.Request.Body = http.MaxBytesReader(input.Context.ResponseWriter, input.Context.Request.Body, maxMemory)
-	if strings.Contains(input.Header("Content-Type"), "multipart/form-data") {
+	if input.IsUpload() {
 		if err := input.Context.Request.ParseMultipartForm(maxMemory); err != nil {
 			return errors.New("Error parsing request body:" + err.Error())
 		}
