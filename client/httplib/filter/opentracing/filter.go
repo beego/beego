ef258a0988ab914a186efe4ea5f8655e73fed0d6
Ming Deng <mindeng@ebay.com>
2020-12-25 22:11:40 +0800

    Support httplib mock

--- client/httplib/filter/opentracing/filter.go
+++ client/httplib/filter/opentracing/filter.go
@@ -21,11 +21,14 @@ import (
 	logKit "github.com/go-kit/kit/log"
 	opentracingKit "github.com/go-kit/kit/tracing/opentracing"
 	"github.com/opentracing/opentracing-go"
+	"github.com/opentracing/opentracing-go/log"
 
 	"github.com/beego/beego/v2/client/httplib"
 )
 
 type FilterChainBuilder struct {
+	// TagURL true will tag span with url
+	TagURL bool
 	// CustomSpanFunc users are able to custom their span
 	CustomSpanFunc func(span opentracing.Span, ctx context.Context,
 		req *httplib.BeegoHTTPRequest, resp *http.Response, err error)
@@ -50,13 +53,19 @@ func (builder *FilterChainBuilder) FilterChain(next httplib.Filter) httplib.Filt
 		}
 		span.SetTag("http.method", method)
 		span.SetTag("peer.hostname", req.GetRequest().URL.Host)
-		span.SetTag("http.url", req.GetRequest().URL.String())
+
 		span.SetTag("http.scheme", req.GetRequest().URL.Scheme)
 		span.SetTag("span.kind", "client")
 		span.SetTag("component", "beego")
+
+		if builder.TagURL {
+			span.SetTag("http.url", req.GetRequest().URL.String())
+		}
+		span.LogFields(log.String("http.url", req.GetRequest().URL.String()))
+
 		if err != nil {
 			span.SetTag("error", true)
-			span.SetTag("message", err.Error())
+			span.LogFields(log.String("message", err.Error()))
 		} else if resp != nil && !(resp.StatusCode < 300 && resp.StatusCode >= 200) {
 			span.SetTag("error", true)
 		}
