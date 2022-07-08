8d0a723c81987f216b90a4b6ef51e6c0c32daa41
Regan Yue <1131625869@qq.com>
2022-07-08 15:31:48 +0800

    Update doc.go

--- adapter/doc.go
+++ adapter/doc.go
@@ -12,5 +12,5 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// used to keep compatible with v1.x
+// Package adapter used to keep compatible with v1.x
 package adapter
