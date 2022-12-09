92795f97df905b612d3cc83ca8cbfe81ef74a34d
Stone-afk <1711865140@qq.com>
2022-12-09 23:16:54 +0800

    feature 增加readthrough

---
+++ client/cache/read_through.go
@@ -0,0 +1,74 @@
+package cache
+
+import (
+	"context"
+	"fmt"
+	"github.com/beego/beego/v2/core/berror"
+	"strings"
+	"sync"
+	"time"
+)
+
+// readThroughCache is a decorator
+// add the read through function to the original Cache function
+type readThroughCache struct {
+	Cache
+	mutex      sync.RWMutex
+	Expiration time.Duration
+	LoadFunc   func(ctx context.Context, key string) (any, error)
+}
+
+// NewReadThroughCache create readThroughCache
+func NewReadThroughCache(cache Cache, expiration time.Duration,
+	loadFunc func(ctx context.Context, key string) (any, error)) (Cache, error) {
+	if loadFunc == nil {
+		return nil, berror.Error(InvalidLoadFunc, "loadFunc cannot be nil")
+	}
+	return &readThroughCache{
+		Cache:      cache,
+		Expiration: expiration,
+		LoadFunc:   loadFunc,
+	}, nil
+}
+
+// Get cache from readThroughCache
+func (c *readThroughCache) Get(ctx context.Context, key string) (any, error) {
+	c.mutex.RLock()
+	val, err := c.Cache.Get(ctx, key)
+	fmt.Println(val)
+	c.mutex.RUnlock()
+	if val == nil || err != nil {
+		c.mutex.Lock()
+		defer c.mutex.Unlock()
+		val, err = c.LoadFunc(ctx, key)
+		if err != nil {
+			return nil, berror.Wrap(
+				err, KeyNotExist, "cache unable to load data")
+		}
+		err = c.Cache.Put(ctx, key, val, c.Expiration)
+		if err != nil {
+			return val, err
+		}
+	}
+	return val, nil
+}
+
+// GetMulti cache from readThroughCache
+func (c *readThroughCache) GetMulti(ctx context.Context, keys []string) ([]any, error) {
+	rc := make([]interface{}, len(keys))
+	keysErr := make([]string, 0)
+
+	for i, ki := range keys {
+		val, err := c.Get(context.Background(), ki)
+		if err != nil {
+			keysErr = append(keysErr, fmt.Sprintf("key [%s] error: %s", ki, err.Error()))
+			continue
+		}
+		rc[i] = val
+	}
+
+	if len(keysErr) == 0 {
+		return rc, nil
+	}
+	return rc, berror.Error(MultiGetFailed, strings.Join(keysErr, "; "))
+}
---
+++ client/cache/read_through_test.go
@@ -0,0 +1,237 @@
+package cache
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"github.com/beego/beego/v2/core/berror"
+	"github.com/stretchr/testify/assert"
+	"strings"
+	"testing"
+	"time"
+)
+
+func TestReadThroughCache_Memory_Get(t *testing.T) {
+	bm, err := NewCache("memory", `{"interval":20}`)
+	assert.Nil(t, err)
+	testReadThroughCacheGet(t, bm)
+
+	testReadThroughCacheGetMulti(t, bm)
+}
+
+func TestReadThroughCache_file_Get(t *testing.T) {
+	fc := NewFileCache().(*FileCache)
+	fc.CachePath = "////aaa"
+	err := fc.Init()
+	assert.NotNil(t, err)
+	fc.CachePath = getTestCacheFilePath()
+	err = fc.Init()
+	assert.Nil(t, err)
+	testReadThroughCacheGet(t, fc)
+
+	testReadThroughCacheGetMulti(t, fc)
+}
+
+func testReadThroughCacheGet(t *testing.T, bm Cache) {
+	testCases := []struct {
+		name    string
+		key     string
+		value   string
+		cache   Cache
+		wantErr error
+	}{
+		{
+			name: "Get load err",
+			key:  "key0",
+			cache: func() Cache {
+				kvs := map[string]any{"key0": "value0"}
+				db := &MockOrm{kvs: kvs}
+				loadfunc := func(ctx context.Context, key string) (any, error) {
+					val, er := db.Load(key)
+					if er != nil {
+						return nil, er
+					}
+					return val, nil
+				}
+				c, err := NewReadThroughCache(bm, 3*time.Second, loadfunc)
+				assert.Nil(t, err)
+				return c
+			}(),
+			wantErr: func() error {
+				err := errors.New("the key not exist")
+				return berror.Wrap(
+					err, KeyNotExist, "cache unable to load data")
+			}(),
+		},
+		{
+			name:  "Get cache exist",
+			key:   "key1",
+			value: "value1",
+			cache: func() Cache {
+				keysMap := map[string]int{"key1": 1}
+				kvs := map[string]any{"key1": "value1"}
+				db := &MockOrm{keysMap: keysMap, kvs: kvs}
+				loadfunc := func(ctx context.Context, key string) (any, error) {
+					val, er := db.Load(key)
+					if er != nil {
+						return nil, er
+					}
+					return val, nil
+				}
+				c, err := NewReadThroughCache(bm, 3*time.Second, loadfunc)
+				assert.Nil(t, err)
+				err = c.Put(context.Background(), "key1", "value1", 3*time.Second)
+				assert.Nil(t, err)
+				return c
+			}(),
+		},
+		{
+			name:  "Get loadFunc exist",
+			key:   "key2",
+			value: "value2",
+			cache: func() Cache {
+				keysMap := map[string]int{"key2": 1}
+				kvs := map[string]any{"key2": "value2"}
+				db := &MockOrm{keysMap: keysMap, kvs: kvs}
+				loadfunc := func(ctx context.Context, key string) (any, error) {
+					val, er := db.Load(key)
+					if er != nil {
+						return nil, er
+					}
+					return val, nil
+				}
+				c, err := NewReadThroughCache(bm, 3*time.Second, loadfunc)
+				assert.Nil(t, err)
+				return c
+			}(),
+		},
+	}
+	_, err := NewReadThroughCache(bm, 3*time.Second, nil)
+	assert.Equal(t, berror.Error(InvalidLoadFunc, "loadFunc cannot be nil"), err)
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			c := tc.cache
+			val, err := c.Get(context.Background(), tc.key)
+			if err != nil {
+				assert.EqualError(t, tc.wantErr, err.Error())
+				return
+			}
+			assert.Equal(t, tc.value, val)
+		})
+
+	}
+}
+
+func testReadThroughCacheGetMulti(t *testing.T, bm Cache) {
+	testCases := []struct {
+		name    string
+		keys    []string
+		values  []any
+		cache   Cache
+		wantErr error
+	}{
+		{
+			name: "GetMulti load err",
+			keys: []string{"key0", "key01"},
+			cache: func() Cache {
+				db := &MockOrm{}
+				loadfunc := func(ctx context.Context, key string) (any, error) {
+					val, er := db.Load(key)
+					if er != nil {
+						return nil, er
+					}
+					return val, nil
+				}
+				c, err := NewReadThroughCache(bm, 3*time.Second, loadfunc)
+				assert.Nil(t, err)
+				return c
+			}(),
+			wantErr: func() error {
+				keysErr := make([]string, 0)
+				err1 := berror.Wrap(
+					errors.New("the key not exist"),
+					KeyNotExist, "cache unable to load data")
+				err2 := berror.Wrap(
+					errors.New("the key not exist"),
+					KeyNotExist, "cache unable to load data")
+				keyErrMap := map[string]error{"key0": err1, "key01": err2}
+				for ki, err := range keyErrMap {
+					keysErr = append(keysErr, fmt.Sprintf("key [%s] error: %s", ki, err.Error()))
+				}
+				return berror.Error(MultiGetFailed, strings.Join(keysErr, "; "))
+
+			}(),
+		},
+		{
+			name:   "GetMulti cache exist",
+			keys:   []string{"key1", "key2"},
+			values: []any{"value1", "value2"},
+			cache: func() Cache {
+				keysMap := map[string]int{"key1": 1, "key2": 1}
+				kvs := map[string]any{"key1": "value1", "key2": "value2"}
+				db := &MockOrm{keysMap: keysMap, kvs: kvs}
+				loadfunc := func(ctx context.Context, key string) (any, error) {
+					val, er := db.Load(key)
+					if er != nil {
+						return nil, er
+					}
+					return val, nil
+				}
+				c, err := NewReadThroughCache(bm, 3*time.Second, loadfunc)
+				assert.Nil(t, err)
+				for key, value := range kvs {
+					err = c.Put(context.Background(), key, value, 3*time.Second)
+					assert.Nil(t, err)
+				}
+				return c
+			}(),
+		},
+		{
+			name:   "GetMulti loadFunc exist",
+			keys:   []string{"key3", "key4"},
+			values: []any{"value3", "value4"},
+			cache: func() Cache {
+				keysMap := map[string]int{"key3": 1, "key4": 1}
+				kvs := map[string]any{"key3": "value3", "key4": "value4"}
+				db := &MockOrm{keysMap: keysMap, kvs: kvs}
+				loadfunc := func(ctx context.Context, key string) (any, error) {
+					val, er := db.Load(key)
+					if er != nil {
+						return nil, er
+					}
+					return val, nil
+				}
+				c, err := NewReadThroughCache(bm, 3*time.Second, loadfunc)
+				assert.Nil(t, err)
+				return c
+			}(),
+		},
+	}
+	_, err := NewReadThroughCache(bm, 3*time.Second, nil)
+	assert.Equal(t, berror.Error(InvalidLoadFunc, "loadFunc cannot be nil"), err)
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			c := tc.cache
+			val, err := c.GetMulti(context.Background(), tc.keys)
+			if err != nil {
+				assert.Equal(t, tc.wantErr, err)
+				return
+			}
+			assert.EqualValues(t, tc.values, val)
+		})
+
+	}
+}
+
+type MockOrm struct {
+	keysMap map[string]int
+	kvs     map[string]any
+}
+
+func (m *MockOrm) Load(key string) (any, error) {
+	_, ok := m.keysMap[key]
+	if !ok {
+		return nil, errors.New("the key not exist")
+	}
+	return m.kvs[key], nil
+}
