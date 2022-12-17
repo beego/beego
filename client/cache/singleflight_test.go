935566b0ba5c47f99661b8e9dfc2efe776504d85
Stone-afk <1711865140@qq.com>
2022-12-17 23:29:25 +0800

    feature motify test

--- client/cache/singleflight_test.go
+++ client/cache/singleflight_test.go
@@ -16,20 +16,16 @@ package cache
 
 import (
 	"context"
-	"errors"
 	"sync"
 	"testing"
 	"time"
 
-	"github.com/beego/beego/v2/core/berror"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestSingleflight_Memory_Get(t *testing.T) {
 	bm, err := NewCache("memory", `{"interval":20}`)
 	assert.Nil(t, err)
-
-	testSingleflightCacheGet(t, bm)
 
 	testSingleflightCacheConcurrencyGet(t, bm)
 }
@@ -43,97 +39,7 @@ func TestSingleflight_file_Get(t *testing.T) {
 	err = fc.Init()
 	assert.Nil(t, err)
 
-	testSingleflightCacheGet(t, fc)
-
 	testSingleflightCacheConcurrencyGet(t, fc)
-}
-
-func testSingleflightCacheGet(t *testing.T, bm Cache) {
-	testCases := []struct {
-		name    string
-		key     string
-		value   string
-		cache   Cache
-		wantErr error
-	}{
-		{
-			name: "Get load err",
-			key:  "key0",
-			cache: func() Cache {
-				kvs := map[string]any{"key0": "value0"}
-				db := &MockOrm{kvs: kvs}
-				loadfunc := func(ctx context.Context, key string) (any, error) {
-					val, er := db.Load(key)
-					if er != nil {
-						return nil, er
-					}
-					return val, nil
-				}
-				c, err := NewSingleflightCache(bm, 3*time.Second, loadfunc)
-				assert.Nil(t, err)
-				return c
-			}(),
-			wantErr: func() error {
-				err := errors.New("the key not exist")
-				return berror.Wrap(err, LoadFuncFailed, "cache unable to load data")
-			}(),
-		},
-		{
-			name:  "Get cache exist",
-			key:   "key1",
-			value: "value1",
-			cache: func() Cache {
-				keysMap := map[string]int{"key1": 1}
-				kvs := map[string]any{"key1": "value1"}
-				db := &MockOrm{keysMap: keysMap, kvs: kvs}
-				loadfunc := func(ctx context.Context, key string) (any, error) {
-					val, er := db.Load(key)
-					if er != nil {
-						return nil, er
-					}
-					return val, nil
-				}
-				c, err := NewSingleflightCache(bm, 3*time.Second, loadfunc)
-				assert.Nil(t, err)
-				err = c.Put(context.Background(), "key1", "value1", 3*time.Second)
-				assert.Nil(t, err)
-				return c
-			}(),
-		},
-		{
-			name:  "Get loadFunc exist",
-			key:   "key2",
-			value: "value2",
-			cache: func() Cache {
-				keysMap := map[string]int{"key2": 1}
-				kvs := map[string]any{"key2": "value2"}
-				db := &MockOrm{keysMap: keysMap, kvs: kvs}
-				loadfunc := func(ctx context.Context, key string) (any, error) {
-					val, er := db.Load(key)
-					if er != nil {
-						return nil, er
-					}
-					return val, nil
-				}
-				c, err := NewSingleflightCache(bm, 3*time.Second, loadfunc)
-				assert.Nil(t, err)
-				return c
-			}(),
-		},
-	}
-	_, err := NewSingleflightCache(bm, 3*time.Second, nil)
-	assert.Equal(t, berror.Error(InvalidLoadFunc, "loadFunc cannot be nil"), err)
-	for _, tc := range testCases {
-		t.Run(tc.name, func(t *testing.T) {
-			c := tc.cache
-			val, err := c.Get(context.Background(), tc.key)
-			if err != nil {
-				assert.EqualError(t, tc.wantErr, err.Error())
-				return
-			}
-			assert.Equal(t, tc.value, val)
-		})
-	}
 }
 
 func testSingleflightCacheConcurrencyGet(t *testing.T, bm Cache) {
