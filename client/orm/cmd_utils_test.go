3673a322a68bb9ad4de257120bb4c9da382be2fb
Deng Ming <ming_flycash@qq.com>
2023-06-20 17:18:21 +0800

    closes 5254: %COL% should be a common placeholder

---
+++ client/orm/cmd_utils_test.go
@@ -0,0 +1,37 @@
+package orm
+
+import (
+	"github.com/beego/beego/v2/client/orm/internal/models"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func Test_getColumnTyp(t *testing.T) {
+	testCases := []struct {
+		name string
+		fi   *models.FieldInfo
+		al   *alias
+
+		wantCol string
+	}{
+		{
+			// https://github.com/beego/beego/issues/5254
+			name: "issue 5254",
+			fi: &models.FieldInfo{
+				FieldType: TypePositiveIntegerField,
+				Column:    "my_col",
+			},
+			al: &alias{
+				DbBaser: newdbBasePostgres(),
+			},
+			wantCol: `bigint CHECK("my_col" >= 0)`,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			col := getColumnTyp(tc.al, tc.fi)
+			assert.Equal(t, tc.wantCol, col)
+		})
+	}
+}
