8a2fd29bbc8bf0ccdbf3ba502c8bf41d355dae76
junwense <970597653@qq.com>
2025-04-07 17:07:12 +0800

    feat: 1.新增web层泛型的支持，支持用户在方法上定义泛型解析http里的各种对象

---
+++ server/web/webx/type_test.go
@@ -0,0 +1,241 @@
+package webx
+
+import (
+	"bytes"
+	"encoding/json"
+	"encoding/xml"
+	"errors"
+	"github.com/beego/beego/v2/server/web"
+	"github.com/beego/beego/v2/server/web/context"
+	"github.com/stretchr/testify/assert"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"strings"
+	"testing"
+)
+
+func TestAllWrapperTestCase(t *testing.T) {
+
+	type MyStruct struct {
+		Foo string `form:"foo" json:"foo"`
+	}
+
+	myStruct := MyStruct{Foo: "bar"}
+	testCases := []struct {
+		name         string
+		expectedCode int
+		expectedRes  func() string
+		reqBody      func() io.Reader
+		contentType  string
+		bizProvider  func() web.HandleFunc
+	}{
+		{
+			name:         "Test post json requestBody",
+			expectedCode: http.StatusOK,
+			expectedRes: func() string {
+				marshal, _ := json.Marshal(myStruct)
+				return string(marshal)
+			},
+			reqBody: func() io.Reader {
+				return strings.NewReader(`{"foo": "bar"}`)
+			},
+			contentType: context.ApplicationJSON,
+			bizProvider: func() web.HandleFunc {
+				testFunc := func(ctx *context.Context, s1 MyStruct) (any, error) {
+					return s1, nil
+				}
+				return WrapperFromJson(testFunc)
+			},
+		},
+		{
+			name:         "Test post form requestBody",
+			expectedCode: http.StatusOK,
+			expectedRes: func() string {
+				marshal, _ := json.Marshal(myStruct)
+				return string(marshal)
+			},
+			reqBody: func() io.Reader {
+				formData := url.Values{}
+				formData.Set("foo", "bar")
+				return strings.NewReader(formData.Encode())
+			},
+			contentType: context.ApplicationForm,
+			bizProvider: func() web.HandleFunc {
+				testFunc := func(ctx *context.Context, s1 MyStruct) (any, error) {
+					return s1, nil
+				}
+				return WrapperFromForm(testFunc)
+			},
+		},
+		{
+			name:         "Test unWrapper error",
+			expectedCode: http.StatusBadRequest,
+			reqBody: func() io.Reader {
+				formData := url.Values{}
+				formData.Set("foo", "bar")
+				return strings.NewReader(formData.Encode())
+			},
+			contentType: context.ApplicationForm,
+			bizProvider: func() web.HandleFunc {
+				testFunc := func(ctx *context.Context, s1 MyStruct) (any, error) {
+					return s1, nil
+				}
+				return wrapper(testFunc, func(ctx *context.Context, params *MyStruct) error {
+					return errors.New("paras entity error")
+				})
+			},
+		},
+		{
+			name:         "Test biz error",
+			expectedCode: http.StatusInternalServerError,
+			reqBody: func() io.Reader {
+				formData := url.Values{}
+				formData.Set("foo", "bar")
+				return strings.NewReader(formData.Encode())
+			},
+			contentType: context.ApplicationForm,
+			bizProvider: func() web.HandleFunc {
+				testFunc := func(ctx *context.Context, s1 MyStruct) (any, error) {
+					return nil, errors.New("biz error")
+				}
+				return WrapperFromForm(testFunc)
+			},
+		},
+		{
+			name:         "Test base binging",
+			expectedCode: http.StatusOK,
+			expectedRes: func() string {
+				marshal, _ := json.Marshal(myStruct)
+				return string(marshal)
+			},
+			reqBody: func() io.Reader {
+				marshal, _ := xml.Marshal(myStruct)
+				return bytes.NewBuffer(marshal)
+			},
+			contentType: context.ApplicationXML,
+			bizProvider: func() web.HandleFunc {
+				testFunc := func(ctx *context.Context, s1 MyStruct) (any, error) {
+					return s1, nil
+				}
+				return Wrapper(testFunc)
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			// 1. init web server
+			app := web.NewHttpSever()
+			// 2. set copy request body
+			app.Cfg.CopyRequestBody = true
+
+			// 3. register route
+			app.Post("/api/data", tc.bizProvider())
+
+			// 4. create request
+			req := httptest.NewRequest("POST", "/api/data", tc.reqBody())
+			req.Header.Set("Content-Type", tc.contentType)
+			req.Header.Set("Accept", "*/*")
+			// 5. create ResponseRecorder
+			w := httptest.NewRecorder()
+
+			// 6. process request
+			app.Handlers.ServeHTTP(w, req)
+
+			assert.Equal(t, tc.expectedCode, w.Code)
+			if w.Code == http.StatusOK {
+				assert.Equal(t, tc.expectedRes(), w.Body.String())
+			}
+		})
+	}
+}
+
+func TestWrapperFromJson(t *testing.T) {
+	type MyStruct1 struct {
+		Foo string `json:"foo"`
+	}
+
+	contentType := "application/json"
+	testFunc := func(ctx *context.Context, s1 MyStruct1) (any, error) {
+		return s1, nil
+	}
+
+	app := web.NewHttpSever()
+	app.Cfg.CopyRequestBody = true
+	app.Post("/api/data", WrapperFromJson(testFunc))
+	reqBody := bytes.NewBuffer([]byte(`{"foo": "bar"}`))
+
+	req := httptest.NewRequest("POST", "/api/data", reqBody)
+	req.Header.Set("Content-Type", contentType)
+	req.Header.Set("Accept", "*/*")
+	w := httptest.NewRecorder()
+
+	app.Handlers.ServeHTTP(w, req)
+
+	assert.Equal(t, http.StatusOK, w.Code, "HTTP 状态码错误")
+	assert.Equal(t, "{\"foo\":\"bar\"}", w.Body.String(), "响应内容错误")
+}
+
+func TestWrapperFromForm(t *testing.T) {
+	type MyStruct1 struct {
+		Foo string `form:"foo" json:"foo"`
+	}
+	contentType := "application/x-www-form-urlencoded"
+
+	testFunc := func(ctx *context.Context, s1 MyStruct1) (any, error) {
+		return s1, nil
+	}
+
+	app := web.NewHttpSever()
+	app.Cfg.CopyRequestBody = true
+	app.Post("/api/data", WrapperFromForm(testFunc))
+
+	formData := url.Values{}
+	formData.Set("foo", "bar")
+
+	req := httptest.NewRequest("POST", "/api/data", strings.NewReader(formData.Encode()))
+	req.Header.Set("Content-Type", contentType)
+	req.Header.Set("Accept", "*/*")
+
+	w := httptest.NewRecorder()
+
+	app.Handlers.ServeHTTP(w, req)
+
+	assert.Equal(t, http.StatusOK, w.Code, "HTTP 状态码错误")
+	assert.Equal(t, "{\"foo\":\"bar\"}", w.Body.String(), "响应内容错误")
+}
+
+func TestWrapper(t *testing.T) {
+	type MyStruct1 struct {
+		Foo string `form:"foo" json:"foo"`
+	}
+
+	myStruct1 := MyStruct1{Foo: "bar"}
+
+	contentType := "application/xml"
+	testFunc := func(ctx *context.Context, s1 MyStruct1) (any, error) {
+		return s1, nil
+	}
+
+	app := web.NewHttpSever()
+	app.Cfg.CopyRequestBody = true
+	app.Post("/api/data", Wrapper(testFunc))
+
+	formData := url.Values{}
+	formData.Set("foo", "bar")
+
+	marshal, _ := xml.Marshal(myStruct1)
+
+	req := httptest.NewRequest("POST", "/api/data", bytes.NewBuffer(marshal))
+	req.Header.Set("Content-Type", contentType)
+	req.Header.Set("Accept", "*/*")
+
+	w := httptest.NewRecorder()
+
+	app.Handlers.ServeHTTP(w, req)
+
+	assert.Equal(t, http.StatusOK, w.Code, "HTTP 状态码错误")
+	assert.Equal(t, "{\"foo\":\"bar\"}", w.Body.String(), "响应内容错误")
+}
