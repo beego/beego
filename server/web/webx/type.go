e76842a117009d9752ba4c907355703dfbeb57a8
junwense <970597653@qq.com>
2025-04-08 09:51:33 +0800

    feature:Added plugin support for generic types at the web.

---
+++ server/web/webx/type.go
@@ -0,0 +1,75 @@
+package webx
+
+import (
+	"github.com/beego/beego/v2/server/web/context"
+)
+
+// WrapperFromJson is a wrapper function for handling JSON in request's body.
+// usage:
+//
+//	web.Post("/hello", WrapperFromJson(func(ctx *context.Context, param T) (any, error) {
+//		 return param, nil
+//	}))
+//
+// It binds the JSON request body to the specified type T
+// See test cases for details
+func WrapperFromJson[T any](
+	biz func(ctx *context.Context, param T) (any, error)) func(ctx *context.Context) {
+	return wrapper(biz, func(ctx *context.Context, params *T) error {
+		return ctx.BindJSON(params)
+	})
+}
+
+// WrapperFromForm is a wrapper function for handling form data in request.
+// usage:
+//
+//	web.Post("/hello", WrapperFromForm(func(ctx *context.Context, param T) (any, error) {
+//		 return param, nil
+//	}))
+//
+// It binds the form data to the specified type T
+// See test cases for details
+func WrapperFromForm[T any](
+	biz func(ctx *context.Context, param T) (any, error)) func(ctx *context.Context) {
+	return wrapper(biz, func(ctx *context.Context, params *T) error {
+		return ctx.BindForm(params)
+	})
+}
+
+// Wrapper is use by beego ctx.Bind(any) api
+// usage:
+//
+//	web.Post("/hello", Wrapper(func(ctx *context.Context, param T) (any, error) {
+//		 return param, nil
+//	}))
+//
+// It binds the data to the specified type T
+// See test cases for details
+func Wrapper[T any](
+	biz func(ctx *context.Context, param T) (any, error)) func(ctx *context.Context) {
+	return wrapper(biz, func(ctx *context.Context, params *T) error {
+		return ctx.Bind(params)
+	})
+}
+
+func wrapper[T any](
+	biz func(ctx *context.Context, param T) (any, error),
+	wf func(ctx *context.Context, params *T) error) func(ctx *context.Context) {
+	return func(ctx *context.Context) {
+		var params T
+		err := wf(ctx, &params)
+		if err != nil {
+			ctx.Abort(400, err.Error())
+			return
+		}
+		res, err := biz(ctx, params)
+		if err != nil {
+			ctx.Abort(500, err.Error())
+			return
+		}
+		err = ctx.Resp(res)
+		if err != nil {
+			panic(err)
+		}
+	}
+}
---
+++ server/web/webx/type_test.go
@@ -0,0 +1,145 @@
+package webx
+
+import (
+	"bytes"
+	"encoding/json"
+	"encoding/xml"
+	"errors"
+	"github.com/beego/beego/v2/server/web"
+	"github.com/beego/beego/v2/server/web/context"
+	"github.com/stretchr/testify/assert"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"strings"
+	"testing"
+)
+
+func TestAllWrapperTestCase(t *testing.T) {
+
+	type MyStruct struct {
+		Foo string `form:"foo" json:"foo"`
+	}
+
+	myStruct := MyStruct{Foo: "bar"}
+	webFunc := func(ctx *context.Context, s1 MyStruct) (any, error) {
+		return s1, nil
+	}
+	testCases := []struct {
+		name         string
+		expectedCode int
+		expectedRes  func() string
+		reqBody      func() io.Reader
+		contentType  string
+		bizProvider  func() web.HandleFunc
+	}{
+		{
+			name:         "Test post json requestBody",
+			expectedCode: http.StatusOK,
+			expectedRes: func() string {
+				marshal, _ := json.Marshal(myStruct)
+				return string(marshal)
+			},
+			reqBody: func() io.Reader {
+				return strings.NewReader(`{"foo": "bar"}`)
+			},
+			contentType: context.ApplicationJSON,
+			bizProvider: func() web.HandleFunc {
+				return WrapperFromJson(webFunc)
+			},
+		},
+		{
+			name:         "Test post form requestBody",
+			expectedCode: http.StatusOK,
+			expectedRes: func() string {
+				marshal, _ := json.Marshal(myStruct)
+				return string(marshal)
+			},
+			reqBody: func() io.Reader {
+				formData := url.Values{}
+				formData.Set("foo", "bar")
+				return strings.NewReader(formData.Encode())
+			},
+			contentType: context.ApplicationForm,
+			bizProvider: func() web.HandleFunc {
+				return WrapperFromForm(webFunc)
+			},
+		},
+		{
+			name:         "Test base binging",
+			expectedCode: http.StatusOK,
+			expectedRes: func() string {
+				marshal, _ := json.Marshal(myStruct)
+				return string(marshal)
+			},
+			reqBody: func() io.Reader {
+				marshal, _ := xml.Marshal(myStruct)
+				return bytes.NewBuffer(marshal)
+			},
+			contentType: context.ApplicationXML,
+			bizProvider: func() web.HandleFunc {
+				return Wrapper(webFunc)
+			},
+		},
+		{
+			name:         "Test unWrapper error",
+			expectedCode: http.StatusBadRequest,
+			reqBody: func() io.Reader {
+				formData := url.Values{}
+				formData.Set("foo", "bar")
+				return strings.NewReader(formData.Encode())
+			},
+			contentType: context.ApplicationForm,
+			bizProvider: func() web.HandleFunc {
+				return wrapper(webFunc, func(ctx *context.Context, params *MyStruct) error {
+					return errors.New("paras entity error")
+				})
+			},
+		},
+		{
+			name:         "Test biz error",
+			expectedCode: http.StatusInternalServerError,
+			reqBody: func() io.Reader {
+				formData := url.Values{}
+				formData.Set("foo", "bar")
+				return strings.NewReader(formData.Encode())
+			},
+			contentType: context.ApplicationForm,
+			bizProvider: func() web.HandleFunc {
+				testFunc := func(ctx *context.Context, s1 MyStruct) (any, error) {
+					return nil, errors.New("biz error")
+				}
+				return WrapperFromForm(testFunc)
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			// 1. init web server
+			app := web.NewHttpSever()
+			// 2. set copy request body
+			app.Cfg.CopyRequestBody = true
+
+			// 3. register route
+			path := "/api/data"
+			app.Post(path, tc.bizProvider())
+
+			// 4. create request
+			req := httptest.NewRequest("POST", path, tc.reqBody())
+			req.Header.Set("Content-Type", tc.contentType)
+			req.Header.Set("Accept", "*/*")
+			// 5. create ResponseRecorder
+			w := httptest.NewRecorder()
+
+			// 6. process request
+			app.Handlers.ServeHTTP(w, req)
+
+			assert.Equal(t, tc.expectedCode, w.Code)
+			if w.Code == http.StatusOK {
+				assert.Equal(t, tc.expectedRes(), w.Body.String())
+			}
+		})
+	}
+}
